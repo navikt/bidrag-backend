6fa5557d50fb3b4e7be1120928280ee85195c01c
Ugur Alpay Cenar <cenar93@gmail.com>
2026-09-18 19:51:40 +0200

    Fikset samvær vurder separat logikk

--- apps/bidrag-behandling/src/main/kotlin/no/nav/bidrag/behandling/database/datamodell/Behandling.kt
+++ apps/bidrag-behandling/src/main/kotlin/no/nav/bidrag/behandling/database/datamodell/Behandling.kt
@@ -420,14 +420,7 @@ open class Behandling(
             sb2.normalisertNotat(NotatGrunnlag.NotatType.VIRKNINGSTIDSPUNKT_VURDERING_AV_SKOLEGANG)
     }
 
-    val sammeSamværForAlle get() =
-        forholdsmessigFordeling == null &&
-            samvær.filter { it.rolle.kreverGrunnlagForBeregning }.all { sb1 ->
-                samvær.filter { it.rolle.kreverGrunnlagForBeregning }.filter { it.id != sb1.id }.all { sb2 ->
-                    sb1.erLik(sb2) && sb1.rolle.virkningstidspunkt == sb2.rolle.virkningstidspunkt &&
-                        sb1.rolle.opphørsdato == sb2.rolle.opphørsdato
-                }
-            }
+    val sammeSamværForAlle get() = sammeSamværForAlleSaker.all { it.erLikForAlle }
     val sammeSamværForAlleSaker get() = samvær
         .filter { it.rolle.kreverGrunnlagForBeregning }
         .groupBy { it.rolle.saksnummer }
